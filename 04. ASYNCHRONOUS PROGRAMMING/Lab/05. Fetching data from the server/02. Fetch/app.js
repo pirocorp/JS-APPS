7714da1ebd6acc54db879d4c6812f6044679547c
Zdravko Zdravkov <zdr0@yahoo.com>
2019-10-14 14:13:25 +0200

    02. Fetch

---
+++ 04. ASYNCHRONOUS PROGRAMMING/Lab/05. Fetching data from the server/02. Fetch/app.js
@@ -0,0 +1,24 @@
+var verseChoose = document.querySelector('select');
+var poemDisplay = document.querySelector('pre');
+
+verseChoose.onchange = function() {
+    var verse = verseChoose.value;
+    updateDisplay(verse);
+};
+
+function updateDisplay(verse) {
+    verse = verse.replace(" ", "");
+    verse = verse.toLowerCase();
+    var url = verse + '.txt';
+
+    fetch(url)
+        .then(function(response) {
+            return response.text();                
+        })
+        .then(function(text) {
+            poemDisplay.textContent = text;
+        });
+};
+
+updateDisplay('Verse 1');
+verseChoose.value = 'Verse 1';
